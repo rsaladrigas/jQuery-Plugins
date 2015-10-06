6ddb4689fb7acedd17ec46150b0553856ea2d51c
Roger Saladrigas <rsaladrigas@outlook.com>
2015-10-06 19:29:05 +0200

    Allow using '.' to insert non standard decimal separator.
    When using a non standard decimal separator, such as comma ',' allow the
    user to insert it using the '.' key.

--- numeric/jquery.numeric.js
+++ numeric/jquery.numeric.js
@@ -143,6 +143,10 @@ $.fn.numeric.keypress = function(e)
 				allow = false;
 			}
 		}
+		//if the key pressed is '.' and the decimal symbol is not present yet, add it to the field.
+		else if(key == 46 && $.inArray(decimal, value.split('')) == -1){
+			this.value += decimal;
+		}
 	}
 	else
 	{
